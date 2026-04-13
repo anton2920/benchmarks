7065c0a98c68c3c2e4e6e2c12976cc5f996344a0
anton2920 <anton2920@gmail.com>
2026-04-13 22:29:45 +0300

    wc: kotlin: added support for '-r' and '-b' flags

--- wc/kotlin/main.kt
+++ wc/kotlin/main.kt
@@ -1,57 +1,58 @@
 import java.io.File
-import java.io.BufferedInputStream
+import java.io.BufferedReader
 
 
-var tnwords = 0; var tnchars = 0; var tnlines = 0
-var nwords = 0; var nchars = 0; var nlines = 0
-var pwords = 0; var pchars = 0; var plines = 0
+var tnwords = 0; var tnchars = 0; var tnrunes = 0; var tnbadrs = 0; var tnlines = 0
+var nwords = 0; var nchars = 0; var nrunes = 0; var nbadrs = 0; var nlines = 0
+var pwords = 0; var pchars = 0; var prunes = 0; var pbadrs = 0; var plines = 0
 
 
-fun wc(stream: BufferedInputStream) {
-	val twoMB = 2 * 1024 * 1024
-	var buf = ByteArray(twoMB)
+fun Boolean.toInt() = if (this) 1 else 0
 
+
+fun wc(stream: BufferedReader) {
+	val Runeerror = 0x80
 	var inWord = false
 
 	nwords = 0
 	nchars = 0
 	nlines = 0
 
-	stream.use { input ->
-		while (true) {
-			var n = input.read(buf)
-			if (n <= 0) {
-				break
-			}
-
-			var i = 0
-			while (i < n) {
-				var c: Char = buf[i].toInt().toChar()
-
-				if ((inWord) && (c.isWhitespace())) {
-					inWord = false
-				} else if ((!inWord) && (!c.isWhitespace())) {
-					inWord = true
-					nwords++
-				}
-
-				if (c == '\n') {
-					nlines++
-				}
-
-				i++
-			}
-
-			nchars += n
+	while (true) {
+		var r = stream.read()
+		if (r == -1) {
+			break;
 		}
+
+		nrunes++
+		if (r == Runeerror) {
+			nbadrs++
+			continue
+		}
+
+		var c = r.toChar()
+		if (c == '\n') {
+			nlines++
+		}
+
+		if ((inWord) && (c.isWhitespace())) {
+			inWord = false
+		} else if ((!inWord) && (!c.isWhitespace())) {
+			inWord = true
+			nwords++
+		}
+
+		nchars += (r > 0xFF).toInt() + 1
 	}
 
 	tnwords += nwords
 	tnchars += nchars
+	tnrunes += nrunes
+	tnbadrs += nbadrs
 	tnlines += nlines
 }
 
-fun report(nwords: Int, nchars: Int, nlines: Int, filename: String) {
+fun report(nwords: Int, nchars: Int, nrunes: Int, nbadrs: Int, nlines: Int, filename: String) {
 	var output = ""
 
 	if (plines > 0) {
@@ -60,6 +61,12 @@ fun report(nwords: Int, nchars: Int, nlines: Int, filename: String) {
 	if (pwords > 0) {
 		output += " ${String.format("%7d", nwords)}"
 	}
+	if (prunes > 0) {
+		output += " ${String.format("%7d", nrunes)}"
+	}
+	if (pbadrs > 0) {
+		output += " ${String.format("%7d", nbadrs)}"
+	}
 	if (pchars > 0) {
 		output += " ${String.format("%7d", nchars)}"
 	}
@@ -67,7 +74,7 @@ fun report(nwords: Int, nchars: Int, nlines: Int, filename: String) {
 		output += " ${filename}"
 	}
 
-	println(output)
+	println(output.slice(1..output.length-1))
 }
 
 fun main(args: Array<String>) {
@@ -78,6 +85,8 @@ fun main(args: Array<String>) {
 			when (arg[1]) {
 				'w' -> pwords++
 				'c' -> pchars++
+				'r' -> prunes++
+				'b' -> pbadrs++
 				'l' -> plines++
 			}
 			i++
@@ -85,7 +94,7 @@ fun main(args: Array<String>) {
 		}
 		break
 	}
-	if (pwords+pchars+plines == 0) {
+	if (pwords+pchars+prunes+pbadrs+plines == 0) {
 		pwords = 1
 		pchars = 1
 		plines = 1
@@ -93,22 +102,22 @@ fun main(args: Array<String>) {
 
 	var nfiles = 0
 	if (i == args.size) {
-		wc(System.`in`.buffered())
-		report(nwords, nchars, nlines, "")
+		wc(System.`in`.bufferedReader())
+		report(nwords, nchars, nrunes, nbadrs, nlines, "")
 	} else {
 		while (i + nfiles < args.size) {
 			var filename = args[i]
 
-			var stream = File(filename).inputStream().buffered()
+			var stream = File(filename).inputStream().bufferedReader()
 			wc(stream)
-			report(nwords, nchars, nlines, filename)
+			report(nwords, nchars, nrunes, nbadrs, nlines, filename)
 			stream.close()
 
 			nfiles++
 		}
 
 		if (nfiles > 1) {
-			report(tnwords, tnchars, tnlines, "total")
+			report(tnwords, tnchars, tnrunes, tnbadrs, tnlines, "total")
 		}
 	}
 }
